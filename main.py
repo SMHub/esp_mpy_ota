86c4233b50c32b5394502edc8a2d6cec830ef907
SMHub <smehbub@outlook.com>
2024-04-28 21:42:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
+# ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
+# ota_updater.download_and_install_update_if_available()
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+
+firmware_url = "https://github.com/SMHub/esp_mpy_ota/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+
 ota_updater.download_and_install_update_if_available()
+
