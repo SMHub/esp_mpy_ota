d30229959ca7a0ed37e8efaabe13454863756196
SMHub <smehbub@outlook.com>
2024-04-29 19:08:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
-# ota_updater.download_and_install_update_if_available()
+
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
-
+from machine import Pin, SoftI2C
+import ssd1306
+from time import sleep
 
 firmware_url = "https://github.com/SMHub/esp_mpy_ota/"
 
@@ -11,4 +12,21 @@ ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
 
 print("Hello World!")
+
+# OLED
+
+
+# ESP32 Pin assignment. Note: 4,5 for OLED with 18630
+i2c = SoftI2C(scl=Pin(4), sda=Pin(5))
+
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
+oled.text('Hello, World 1!', 0, 0)
+oled.text('Hello, World 2!', 0, 20)
+oled.text('Hello, World 3!', 0, 40)
+        
+oled.show()
+
 
