a2c2695b19abd57af20d3c3055d15a9d248fed34
SMHub <smehbub@outlook.com>
2024-04-30 17:04:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ oled_width = 128
 oled_height = 64
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 oled.fill(0)
-oled.text('YES!', 0, 0)
+oled.text('FUN!', 0, 0)
 oled.text('TUE!', 0, 20)
 oled.text('5:02!', 0, 40)
         
